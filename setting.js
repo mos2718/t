412235e40a58888797085852ec8d9da43c396aef
mos2718 <mos2718@gmail.com>
2020-12-30 09:35:54 +0800

    C

--- setting.js
+++ setting.js
@@ -1,5 +1,6 @@
 ﻿var FHIRrootURL = "http://hapi.fhir.org/baseR4";
-/* references
+/*  不會用到
+references
 https://mos2718.github.io/MICexp/Index.html
 https://mos2718.github.io/FHIRclinicalReport/Index.html
 
